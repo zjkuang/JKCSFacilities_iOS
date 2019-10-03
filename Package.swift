d5f0b6887f7892a9b3c8a1a839754fcceff846bb
John Kuang <john.kuang@robotsandpencils.com>
2019-10-03 09:07:06 -0600

    test

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JKCSFacilities_iOS",
-            dependencies: ["UIKit"]),
+            dependencies: []),
         .testTarget(
             name: "JKCSFacilities_iOSTests",
             dependencies: ["JKCSFacilities_iOS"]),
