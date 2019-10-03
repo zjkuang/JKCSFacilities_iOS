3cc65a1c9bb644bfc20c95e1e6294516e0e7b23a
John Kuang <john.kuang@robotsandpencils.com>
2019-10-03 08:45:46 -0600

    public

--- Sources/JKCSFacilities_iOS/JKCSFacilities_iOS.swift
+++ Sources/JKCSFacilities_iOS/JKCSFacilities_iOS.swift
@@ -1,7 +1,15 @@
-struct JKCSFacilities_iOS {
+public struct JKCSFacilities_iOS {
     var text = "Hello, World!"
+    
+    public init() {
+        
+    }
 }
 
 public class JKCSFacilitiesTestClass {
     var text = "Hello, World!"
+    
+    public init() {
+        
+    }
 }
