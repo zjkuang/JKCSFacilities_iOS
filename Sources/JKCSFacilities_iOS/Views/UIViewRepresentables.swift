b5506ec72f37c958c58709ad03238301499887e1
John Kuang <john.kuang@robotsandpencils.com>
2019-10-05 21:34:49 -0600

    UIViewRepresentables

--- Sources/JKCSFacilities_iOS/Views/UIViewRepresentables.swift
+++ Sources/JKCSFacilities_iOS/Views/UIViewRepresentables.swift
@@ -9,26 +9,13 @@ import SwiftUI
 
 public struct ActivityIndicatorView: UIViewRepresentable {
     public var uiActivityIndicatorView = UIActivityIndicatorView()
-    
-    public init() {
-        
-    }
+    @Binding var isAnimating: Bool
     
     public func makeUIView(context: Context) -> UIActivityIndicatorView {
         return uiActivityIndicatorView
     }
     
     public func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
-        
-    }
-    
-    public func startAnimating() -> Self {
-        uiActivityIndicatorView.startAnimating()
-        return self
-    }
-    
-    public func stopAnimating() -> Self {
-        uiActivityIndicatorView.stopAnimating()
-        return self
+        $isAnimating.wrappedValue ? uiView.startAnimating() : uiView.stopAnimating()
     }
 }
