af46596d5577eed4e11e62a9b1f53ed1e06de49e
John Kuang <john.kuang@robotsandpencils.com>
2019-10-06 08:56:26 -0600

    RESTful

--- Sources/JKCSFacilities_iOS/Services/RESTfulService.swift
+++ Sources/JKCSFacilities_iOS/Services/RESTfulService.swift
@@ -54,4 +54,151 @@ public class RESTfulService {
             .eraseToAnyPublisher()
     }
     
+    public func post<T: Decodable>(urlString: String, body: [String: Any]? = nil, headers: [String: String]? = nil, respondObjectType: T.Type) -> AnyPublisher<T, JKCS_RESTful_Error> {
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL: \(urlString)")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        if let body = body,
+            let data = Data(withJSONObject: body)
+        {
+            request.httpBody = data
+        }
+        if let headers = headers {
+            request.allHTTPHeaderFields = headers
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw JKCS_RESTful_Error.invalidResponse
+                }
+                guard httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
+                    // https://www.restapitutorial.com/httpstatuscodes.html
+                    // 1xx Informational, 2xx Success, 3xx Redirection, 4xx Client Error, 5xx Server Error
+                    throw JKCS_RESTful_Error.httpStatusError
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { error in
+                if let error = error as? JKCS_RESTful_Error {
+                    return error
+                }
+                return .genericError
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    public func put<T: Decodable>(urlString: String, body: [String: Any]? = nil, headers: [String: String]? = nil, respondObjectType: T.Type) -> AnyPublisher<T, JKCS_RESTful_Error> {
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL: \(urlString)")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        if let body = body,
+            let data = Data(withJSONObject: body)
+        {
+            request.httpBody = data
+        }
+        if let headers = headers {
+            request.allHTTPHeaderFields = headers
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw JKCS_RESTful_Error.invalidResponse
+                }
+                guard httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
+                    // https://www.restapitutorial.com/httpstatuscodes.html
+                    // 1xx Informational, 2xx Success, 3xx Redirection, 4xx Client Error, 5xx Server Error
+                    throw JKCS_RESTful_Error.httpStatusError
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { error in
+                if let error = error as? JKCS_RESTful_Error {
+                    return error
+                }
+                return .genericError
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    public func patch<T: Decodable>(urlString: String, body: [String: Any]? = nil, headers: [String: String]? = nil, respondObjectType: T.Type) -> AnyPublisher<T, JKCS_RESTful_Error> {
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL: \(urlString)")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"
+        if let body = body,
+            let data = Data(withJSONObject: body)
+        {
+            request.httpBody = data
+        }
+        if let headers = headers {
+            request.allHTTPHeaderFields = headers
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw JKCS_RESTful_Error.invalidResponse
+                }
+                guard httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
+                    // https://www.restapitutorial.com/httpstatuscodes.html
+                    // 1xx Informational, 2xx Success, 3xx Redirection, 4xx Client Error, 5xx Server Error
+                    throw JKCS_RESTful_Error.httpStatusError
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { error in
+                if let error = error as? JKCS_RESTful_Error {
+                    return error
+                }
+                return .genericError
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    public func delete<T: Decodable>(urlString: String, headers: [String: String]? = nil, respondObjectType: T.Type) -> AnyPublisher<T, JKCS_RESTful_Error> {
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL: \(urlString)")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        if let headers = headers {
+            request.allHTTPHeaderFields = headers
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw JKCS_RESTful_Error.invalidResponse
+                }
+                guard httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
+                    // https://www.restapitutorial.com/httpstatuscodes.html
+                    // 1xx Informational, 2xx Success, 3xx Redirection, 4xx Client Error, 5xx Server Error
+                    throw JKCS_RESTful_Error.httpStatusError
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { error in
+                if let error = error as? JKCS_RESTful_Error {
+                    return error
+                }
+                return .genericError
+            }
+            .eraseToAnyPublisher()
+    }
+    
 }
