aa730ace52b4dde35650c76a8c813c17afbbb1b8
John Kuang <john.kuang@robotsandpencils.com>
2020-01-03 20:55:13 -0600

    Bundle

---
+++ Sources/JKCSFacilities_iOS/Extensions/BundleExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  BundleExtensions.swift
+//  
+//
+//  Created by Zhengqian Kuang on 2020-01-03.
+//
+
+import Foundation
+
+extension Bundle {
+    
+    var displayName: String? {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+    
+    var releaseVersionNumber: String? {
+        return object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+    
+    var buildVersionNumber: String? {
+        return object(forInfoDictionaryKey: "CFBundleVersion") as? String
+    }
+    
+}
