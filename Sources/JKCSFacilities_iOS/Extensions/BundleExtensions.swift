4f81a688c3307e740a11a6719893dfe47cab799d
John Kuang <john.kuang@robotsandpencils.com>
2020-01-03 20:59:18 -0600

    Bundle

--- Sources/JKCSFacilities_iOS/Extensions/BundleExtensions.swift
+++ Sources/JKCSFacilities_iOS/Extensions/BundleExtensions.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Bundle {
+public extension Bundle {
     
     var displayName: String? {
         return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? object(forInfoDictionaryKey: "CFBundleName") as? String
