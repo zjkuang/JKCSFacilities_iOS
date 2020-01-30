60bacc1dd55033ef0684f6da3c714a54beab03ce
Zhengqian Kuang <mrkuang@gmail.com>
2020-01-30 17:43:22 -0600

    UILabelExtensions

---
+++ Sources/JKCSFacilities_iOS/Extensions/UILabelExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UILabelExtensions.swift
+//  
+//
+//  Created by Zhengqian Kuang on 2020-01-30.
+//
+
+import UIKit
+
+public extension UILabel {
+    
+    func setFontSize(to fontSize: CGFloat) {
+        self.font = self.font.withSize(fontSize)
+    }
+    
+}
