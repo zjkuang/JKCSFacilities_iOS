f9dfaaf43dd8870422aeadb150a888b6eee7550b
John Kuang <john.kuang@robotsandpencils.com>
2020-05-07 11:33:18 -0600

    substrings extension

--- Sources/JKCSFacilities_iOS/Extensions/StringExtensions.swift
+++ Sources/JKCSFacilities_iOS/Extensions/StringExtensions.swift
@@ -34,3 +34,18 @@ public extension String {
     }
     
 }
+
+// Substrings https://stackoverflow.com/a/46133083/5424189
+public extension String {
+    subscript(_ range: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let end = index(start, offsetBy: min(self.count - range.lowerBound,
+                                             range.upperBound - range.lowerBound))
+        return String(self[start..<end])
+    }
+
+    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+         return String(self[start...])
+    }
+}
